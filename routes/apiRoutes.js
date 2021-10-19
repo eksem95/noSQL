d3aacc4437f4185e9102e2967e10b1182724cdfd
Megan Ekse <meganekse@gmail.com>
2021-10-19 13:23:51 -0700

    api post and put routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,35 @@
 const router = require('express').Router();
-const workout = require('../models/workout');
+const Workout = require('../models/workout');
 
 router.get('/api/workouts', (req, res) => {
-    workout.aggregate().then((data) => {
+    Workout.aggregate().then((data) => {
         console.log(data)
         res.json(data)
     })
     .catch((error) => {
         res.json(error)
     })
+});
+//create workout
+router.post('/api/workouts', ({ body }, res) => {
+    Workout.create(body)
+    .then((data) => {
+        console.log(data)
+        res.json(data)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
+});
+//create new exercise
+router.put('/api/workouts/:id', ({ body }, res) => {
+    Workout.insertMany(body)
+    .then((data) => {
+        res.json(data)
+    })
 })
 
+router.get('/api/workouts/range', (req,res) => {
+    
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -18,11 +18,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
